a601a02149cf2f44c9d03b3c61c6f1a31ff26229
gawgaw <gawgaw.developer@gmail.com>
2020-11-25 23:29:06 +0900

    ログインの状態によってメニューを出し分け

--- assets/_inc/_header.php
+++ assets/_inc/_header.php
@@ -9,13 +9,13 @@
         </a>
       </div>
       <div class="gnavi">
-        <ul class="gnavi__list">
-          <li class="gnavi__item <?php if ($pagerole === 'feed') { echo 'gnavi__item--active';} ?>"><a class="gnavi__link" href="/cafe-map_ogawa_08/index.php"><span>ホーム</span></a></li>
-          <li class="gnavi__item <?php if ($pagerole === 'dispbook') { echo 'gnavi__item--active';} ?>"><a class="gnavi__link" href="/cafe-map_ogawa_08/book_disp.php"><span>おすすめ書籍をさがす</span></a></li>
-          <li class="gnavi__item <?php if ($pagerole === 'addbook') { echo 'gnavi__item--active';} ?>"><a class="gnavi__link" href="/cafe-map_ogawa_08/book_add.php"><span>本を登録する</span></a></li>
-          <li class="gnavi__item <?php if ($pagerole === 'dispcafe') { echo 'gnavi__item--active';} ?>"><a class="gnavi__link" href="/cafe-map_ogawa_08/cafe_disp.php"><span>朝カフェをさがす</span></a></li>
-          <li class="gnavi__item <?php if ($pagerole === 'addcafe') { echo 'gnavi__item--active';} ?>"><a class="gnavi__link" href="/cafe-map_ogawa_08/cafe_add.php"><span>朝カフェを登録する</span></a></li>
-        </ul>
+      <?php
+        if(!isset($_SESSION['login_user']['user_id'])){
+          include('_logout-gnavi.php');
+        } else {
+          include('_login-gnavi.php');
+        }
+      ?>
       </div>
       <div class="header__right">
         <?php
